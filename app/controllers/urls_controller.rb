d7bcdacd9b7ab0e1458b9e975619aab3c5d067d5
Rajesh <rajeshupwk@gmail.com>
2020-11-11 14:21:53 +0530

    Create Url record

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,19 @@
 class UrlsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    @url = Url.new(url_params)
+
+    @url.save
+    redirect_to @url
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:full_url)
+  end
 end

b20880722a8b65f725cd1ba9b01454f926340688
Rajesh <rajeshupwk@gmail.com>
2020-11-11 16:31:50 +0530

    Create short url slug and redirect on full_url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,15 +5,34 @@ class UrlsController < ApplicationController
   end
 
   def create
-    @url = Url.new(url_params)
-
+    @url           = Url.new(url_params)
+    @url.short_url = get_slug
+    @get_short_url = get_short_url
     @url.save
-    redirect_to @url
+  end
+
+  def shortener
+    @url = Url.find_by(short_url: params[:slug])
+
+    if @url.present?
+      redirect_to @url.full_url
+    else
+      flash[:error] = 'page not found.'
+      redirect_to root_url
+    end
   end
 
   private
 
   def url_params
     params.require(:url).permit(:full_url)
+  end
+
+  def get_slug
+    @get_slug ||= rand(36**8).to_s(36)
+  end
+
+  def get_short_url
+    request.base_url + '/s/' + get_slug
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :urls
 
   root 'urls#new'
+
+  get '/s/:slug', to: 'urls#shortener'
 end

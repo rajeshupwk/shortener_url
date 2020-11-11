bffaaad2d43e47dc210a8a68eec319f8d25cc219
Rajesh <rajeshupwk@gmail.com>
2020-11-11 14:01:03 +0530

    Add table Url

---
+++ db/migrate/20201111082749_create_urls.rb
@@ -0,0 +1,11 @@
+class CreateUrls < ActiveRecord::Migration[5.2]
+  def change
+    create_table :urls do |t|
+      t.string :title
+      t.string :full_url
+      t.string :short_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/urls.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :url do
+    title "MyString"
+    full_url "MyString"
+    short_url "MyString"
+  end
+end
